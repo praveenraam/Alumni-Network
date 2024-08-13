433f94d6b7c0dc4ab61fc1c0587749d83ba4e384
PraveenRaamC <ckpraveenraam@gmail.com>
2024-08-13 15:08:54 +0530

    #19, Header is updated

--- Alumni-Project/resources/views/layouts/includes/header.blade.php
+++ Alumni-Project/resources/views/layouts/includes/header.blade.php
@@ -1,261 +1,150 @@
 <div class="topbar stick">
-    <div class="logo">
-      <a title href="newsfeed.html"><img src="images/logo2.png" alt /></a>
-    </div>
-    <div class="top-area">
-      <ul class="main-menu">
-        <li>
-          <a href="#" title>Home</a>
-          <ul>
-            {{-- <li><a href="index.html" title>Home Social</a></li>
-            <li><a href="index2.html" title>Home Social 2</a></li>
-            <li><a href="index-company.html" title>Home Company</a></li>
-            <li><a href="landing.html" title>Login page</a></li>
-            <li><a href="logout.html" title>Logout Page</a></li> --}}
-            <li><a href="newsfeed.html" title>news feed</a></li>
-          </ul>
-        </li>
-        <li>
-          <a href="#" title>timeline</a>
-          <ul>
-            <li><a href="time-line.html" title>timeline</a></li>
-            <li>
-              <a href="timeline-friends.html" title>timeline friends</a>
-            </li>
-            <li>
-              <a href="timeline-groups.html" title>timeline groups</a>
-            </li>
-            <li><a href="timeline-pages.html" title>timeline pages</a></li>
-            <li>
-              <a href="timeline-photos.html" title>timeline photos</a>
-            </li>
-            <li>
-              <a href="timeline-videos.html" title>timeline videos</a>
-            </li>
-            <li><a href="fav-page.html" title>favourit page</a></li>
-            <li><a href="groups.html" title>groups page</a></li>
-            <li><a href="page-likers.html" title>Likes page</a></li>
-            <li><a href="people-nearby.html" title>people nearby</a></li>
-          </ul>
-        </li>
-        <li>
-          <a href="#" title>account settings</a>
-          <ul>
-            <li>
-              <a href="create-fav-page.html" title>create fav page</a>
-            </li>
-            <li>
-              <a href="edit-account-setting.html" title
-                >edit account setting</a
-              >
-            </li>
-            <li><a href="edit-interest.html" title>edit-interest</a></li>
-            <li><a href="edit-password.html" title>edit-password</a></li>
-            <li>
-              <a href="edit-profile-basic.html" title
-                >edit profile basics</a
-              >
-            </li>
-            <li>
-              <a href="edit-work-eductation.html" title
-                >edit work educations</a
-              >
-            </li>
-            <li><a href="messages.html" title>message box</a></li>
-            <li><a href="inbox.html" title>Inbox</a></li>
-            <li>
-              <a href="notifications.html" title>notifications page</a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a href="#" title>more pages</a>
-          <ul>
-            <li><a href="404.html" title>404 error page</a></li>
-            <li><a href="about.html" title>about</a></li>
-            <li><a href="contact.html" title>contact</a></li>
-            <li><a href="faq.html" title>faq's page</a></li>
-            <li><a href="insights.html" title>insights</a></li>
-            <li><a href="knowledge-base.html" title>knowledge base</a></li>
-            <li><a href="widgets.html" title>Widgts</a></li>
-          </ul>
-        </li>
-      </ul>
-      <ul class="setting-area">
-        <li>
-          <a href title="Home" data-ripple><i class="ti-search"></i></a>
-          <div class="searched">
-            <form method="post" class="form-search">
-              <input type="text" placeholder="Search Friend" />
-              <button data-ripple><i class="ti-search"></i></button>
-            </form>
-          </div>
-        </li>
-        <li>
-          <a href="newsfeed.html" title="Home" data-ripple
-            ><i class="ti-home"></i
-          ></a>
-        </li>
-        <li>
-          <a href="#" title="Notification" data-ripple>
-            <i class="ti-bell"></i><span>20</span>
-          </a>
-          <div class="dropdowns">
-            <span>4 New Notifications</span>
-            <ul class="drops-menu">
-              <li>
-                <a href="notifications.html" title>
-                  <img src="images/resources/thumb-1.jpg" alt />
-                  <div class="mesg-meta">
-                    <h6>sarah Loren</h6>
-                    <span>Hi, how r u dear ...?</span>
-                    <i>2 min ago</i>
-                  </div>
-                </a>
-                <span class="tag green">New</span>
-              </li>
-              <li>
-                <a href="notifications.html" title>
-                  <img src="images/resources/thumb-2.jpg" alt />
-                  <div class="mesg-meta">
-                    <h6>Jhon doe</h6>
-                    <span>Hi, how r u dear ...?</span>
-                    <i>2 min ago</i>
-                  </div>
-                </a>
-                <span class="tag red">Reply</span>
-              </li>
-              <li>
-                <a href="notifications.html" title>
-                  <img src="images/resources/thumb-3.jpg" alt />
-                  <div class="mesg-meta">
-                    <h6>Andrew</h6>
-                    <span>Hi, how r u dear ...?</span>
-                    <i>2 min ago</i>
-                  </div>
-                </a>
-                <span class="tag blue">Unseen</span>
-              </li>
-              <li>
-                <a href="notifications.html" title>
-                  <img src="images/resources/thumb-4.jpg" alt />
-                  <div class="mesg-meta">
-                    <h6>Tom cruse</h6>
-                    <span>Hi, how r u dear ...?</span>
-                    <i>2 min ago</i>
-                  </div>
-                </a>
-                <span class="tag">New</span>
-              </li>
-              <li>
-                <a href="notifications.html" title>
-                  <img src="images/resources/thumb-5.jpg" alt />
-                  <div class="mesg-meta">
-                    <h6>Amy</h6>
-                    <span>Hi, how r u dear ...?</span>
-                    <i>2 min ago</i>
-                  </div>
-                </a>
-                <span class="tag">New</span>
-              </li>
-            </ul>
-            <a href="notifications.html" title class="more-mesg"
-              >view more</a
-            >
-          </div>
-        </li>
-        <li>
-          <a href="#" title="Messages" data-ripple
-            ><i class="ti-comment"></i><span>12</span></a
-          >
-          <div class="dropdowns">
-            <span>5 New Messages</span>
-            <ul class="drops-menu">
-              <li>
-                <a href="notifications.html" title>
-                  <img src="images/resources/thumb-1.jpg" alt />
-                  <div class="mesg-meta">
-                    <h6>sarah Loren</h6>
-                    <span>Hi, how r u dear ...?</span>
-                    <i>2 min ago</i>
-                  </div>
-                </a>
-                <span class="tag green">New</span>
-              </li>
-              <li>
-                <a href="notifications.html" title>
-                  <img src="images/resources/thumb-2.jpg" alt />
-                  <div class="mesg-meta">
-                    <h6>Jhon doe</h6>
-                    <span>Hi, how r u dear ...?</span>
-                    <i>2 min ago</i>
-                  </div>
-                </a>
-                <span class="tag red">Reply</span>
-              </li>
-              <li>
-                <a href="notifications.html" title>
-                  <img src="images/resources/thumb-3.jpg" alt />
-                  <div class="mesg-meta">
-                    <h6>Andrew</h6>
-                    <span>Hi, how r u dear ...?</span>
-                    <i>2 min ago</i>
-                  </div>
-                </a>
-                <span class="tag blue">Unseen</span>
-              </li>
-              <li>
-                <a href="notifications.html" title>
-                  <img src="images/resources/thumb-4.jpg" alt />
-                  <div class="mesg-meta">
-                    <h6>Tom cruse</h6>
-                    <span>Hi, how r u dear ...?</span>
-                    <i>2 min ago</i>
-                  </div>
-                </a>
-                <span class="tag">New</span>
-              </li>
-              <li>
-                <a href="notifications.html" title>
-                  <img src="images/resources/thumb-5.jpg" alt />
-                  <div class="mesg-meta">
-                    <h6>Amy</h6>
-                    <span>Hi, how r u dear ...?</span>
-                    <i>2 min ago</i>
-                  </div>
-                </a>
-                <span class="tag">New</span>
-              </li>
-            </ul>
-            <a href="messages.html" title class="more-mesg">view more</a>
-          </div>
-        </li>
-        <li>
-          <a href="#" title="Languages" data-ripple
-            ><i class="fa fa-globe"></i
-          ></a>
-          <div class="dropdowns languages">
-            <a href="#" title><i class="ti-check"></i>English</a>
-            <a href="#" title>Arabic</a>
-            <a href="#" title>Dutch</a>
-            <a href="#" title>French</a>
-          </div>
-        </li>
-      </ul>
-      <div class="user-img">
-        <img src="images/resources/admin.jpg" alt />
-        <span class="status f-online"></span>
-        <div class="user-setting">
-          <a href="#" title><span class="status f-online"></span>online</a>
-          <a href="#" title><span class="status f-away"></span>away</a>
-          <a href="#" title><span class="status f-off"></span>offline</a>
-          <a href="#" title><i class="ti-user"></i> view profile</a>
-          <a href="#" title><i class="ti-pencil-alt"></i>edit profile</a>
-          <a href="#" title><i class="ti-target"></i>activity log</a>
-          <a href="#" title><i class="ti-settings"></i>account setting</a>
-          <a href="#" title><i class="ti-power-off"></i>log out</a>
-        </div>
-      </div>
-      <span class="ti-menu main-menu" data-ripple></span>
-    </div>
+  <div class="logo">
+      {{-- <a title href="#"><img src="{{ asset('images/logo2.png') }}" alt="Logo" /></a> --}}
   </div>
+  <div class="top-area">
+      <ul class="main-menu">
+
+          {{-- Section for Student --}}
+          @if(Auth::guard('student')->check())
+              <li>
+                <a href="{{ url('student/') }}" title>News Feed</a>
+              </li>
+              <li>
+                <a href="#" title>Profile</a>
+                <ul>
+                    <li><a href="{{ url('student/myprofile') }}" title>View Profile</a></li>
+                    <li><a href="{{ url('/student/settings') }}" title>Settings</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#" title>Alumni</a>
+                <ul>
+                    <li><a href="{{ url('student/alumni') }}" title>List</a></li>
+                    <li><a href="{{ url('student/availablementors') }}" title>Mentor Available</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="{{ url('student/jobs') }}" title>Jobs</a>
+            </li>
+            <li>
+                <a href="{{ url('student/events') }}" title>Events</a>
+            </li>
+            <li>
+                <a href="{{ url('student/student-tasks') }}" title>Tasks</a>
+
+            </li>
+          {{-- Section for Alumni --}}
+          @elseif(Auth::guard('alumni')->check())
+              {{-- <li>
+                  <a href="#" title>Alumni Home</a>
+                  <ul>
+                      <li><a href="{{ url('alumni/dashboard') }}" title>Dashboard</a></li>
+                  </ul>
+              </li> --}}
+              <li>
+                <a href="{{ url('alumni/') }}" title>News Feed</a>
+              </li>
+              <li>
+                <a href="#" title>Profile</a>
+                <ul>
+                    <li><a href="{{ url('alumni/myprofile') }}" title>View Profile</a></li>
+                    <li><a href="{{ url('alumni/settings') }}" title>Settings</a></li>
+                    <li><a href="{{ url('alumni/change-password') }}" title>Change Password</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#" title>Alumni</a>
+                <ul>
+                    <li><a href="{{ url('alumni/alumni') }}" title>List</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#" title>Students</a>
+                <ul>
+                    <li><a href="{{ url('alumni/students') }}" title>List</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#" title>Jobs</a>
+                <ul>
+                    <li><a href="{{ url('alumni/jobs') }}" title>List</a></li>
+                    <li><a href="{{ url('alumni/jobs/form') }}" title>Create</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#" title>Events</a>
+                <ul>
+                    <li><a href="{{ url('alumni/events') }}" title>List</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#" title>Tasks</a>
+                <ul>
+                    <li><a href="{{ url('alumni/tasks') }}" title>List</a></li>
+                    <li><a href="{{ url('alumni/tasks/create') }}" title>Create</a></li>
+                </ul>
+              </li>
+          {{-- Section for Admin --}}
+          @elseif(Auth::guard('admin')->check())
+                <li>
+                    <a href="{{ url('admin/') }}" title>NewsFeed</a>
+                </li>
+                <li>
+                  <a href="#" title>Alumni</a>
+                  <ul>
+                      <li><a href="{{ url('admin/alumni') }}" title>List</a></li>
+                      <li><a href="{{ url('/admin/alumni/create') }}" title>Create</a></li>
+                  </ul>
+                </li>
+                <li>
+                    <a href="#" title>Students</a>
+                    <ul>
+                        <li><a href="{{ url('/admin/students') }}" title>List</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#" title>Events</a>
+                    <ul>
+                        <li><a href="{{ url('admin/events') }}" title>List</a></li>
+                        <li><a href="{{ url('admin/events/create') }}" title>Create</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#" title>Jobs</a>
+                    <ul>
+                        <li><a href="{{ url('/admin/students') }}" title>List</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="{{ url('admin/Reports/Post') }}" title>Reports</a>
+                </li>
+                <li>
+                    <a href="{{ url('/admin/tasks') }}" title>All tasks</a>
+                </li>
+                <li>
+                    <a href="{{ url('/admin/forgot-password-request') }}" title>Reset Password List</a>
+                </li>
+          {{-- Guest Section --}}
+          @else
+              <li>
+                  <a href="{{ route('login') }}" title>Login</a>
+              </li>
+              <li>
+                  <a href="{{ route('register') }}" title>Register</a>
+              </li>
+          @endif
+
+      </ul>
+      <ul class="setting-area"> 
+          @if(Auth::guard('student')->check() || Auth::guard('alumni')->check() || Auth::guard('admin')->check())
+              <li>
+                <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <button type="submit" class="btn btn-danger">Logout</button>
+                </form> 
+              </li>
+          @endif
+      </ul>
+  </div>
+</div>
